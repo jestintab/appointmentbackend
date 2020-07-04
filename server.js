0070e7ac9c999476285e23985819c1edbec3e211
IT Aljedad <jestintab@gmail.com>
2020-07-04 19:01:58 +0300

    ready to heroku

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = process.env.MONGO_URI;
+const uri = 'mongodb+srv://mongo_jestin:jesty123@cluster0.iervc.gcp.mongodb.net/test?retryWrites=true&w=majority';
 mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true , useUnifiedTopology: true }
 );
 const connection = mongoose.connection;
@@ -25,6 +25,9 @@ const providersRouter = require('./routes/providers');
 app.use('/users', usersRouter);
 app.use('/providers', providersRouter);
 
+app.get("/", function (req, res) {
+    res.send("<h1>Hello World!</h1>")
+})
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
